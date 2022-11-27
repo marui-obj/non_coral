ba397bb7e4f71a0d3b2ee0395f3d155c903e3dfb
Marui <s6201012610044@email.kmutnb.ac.th>
2022-11-27 16:18:06 +0700

    hint popup

--- app/src/main/java/com/example/noncoralmobile/SelectModeActivity.kt
+++ app/src/main/java/com/example/noncoralmobile/SelectModeActivity.kt
@@ -2,14 +2,26 @@ package com.example.noncoralmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.LineGraphSeries
-import kotlin.math.sin
+import android.view.LayoutInflater
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 
 class SelectModeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_mode)
+
+        val dialogBtnClick: ImageButton = findViewById(R.id.imageButton)
+
+        dialogBtnClick.setOnClickListener{
+            val view = LayoutInflater.from(this).inflate(R.layout.dialog_popup, null)
+
+            val alert = AlertDialog.Builder(this)
+            alert.setView(view)
+            val dialog = alert.create()
+
+            dialog.show()
+        }
     }
 }
